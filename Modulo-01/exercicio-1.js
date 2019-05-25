d27b743cdeabbeb513363dea330f3e7c3474b096
Bruno Ramires <brunormferreira@hotmail.com>
2019-05-25 18:40:43 -0300

    exercicio 01 realizado

---
+++ Modulo-01/exercicio-1.js
@@ -0,0 +1,26 @@
+var endereco = {
+    rua: "Rua dos pinheiros",
+    numero: 1293,
+    bairro: "Centro",
+    cidade: "São Paulo",
+    uf: "SP"
+};
+
+function exibirmsg(endereco) {
+    return (
+        " O usuário mora em " +
+        endereco.cidade +
+        " / " +
+        endereco.uf +
+        ", no bairro " +
+        endereco.bairro +
+        ', na rua "' +
+        endereco.rua +
+        '" com n° ' +
+        endereco.numero +
+        "."
+    );
+};
+
+console.log(exibirmsg(endereco));
+
